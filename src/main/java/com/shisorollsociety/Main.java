ab3862baf14dd75a972cd7fc1e7852e7252521fb
xorgnz <xorgnz@gmail.com>
2013-06-14 15:12:33 -0700

    Initial setup

---
+++ src/main/java/com/shisorollsociety/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.shisorollsociety;
+
+/**
+ * @author xorgnz
+ *
+ */
+public class Main
+{
+    public static void main (String[] argv)
+    {
+        System.err.println("Hello World!");
+    }
+}
